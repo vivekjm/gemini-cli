be3b6f6fd389b189cfc70a409ea47716422b9f37
Adam Weidman <adamfweidman@google.com>
2026-05-12 18:06:50 -0400

    chore(core): remove redundant getters from config in agent-tool branch

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -2585,10 +2585,6 @@ export class Config implements McpContext, AgentLoopContext {
     return this.experimentalContextManagementConfig;
   }
 
-  isAgentSessionEnabled(): boolean {
-    return this.agentSessionSubagentEnabled;
-  }
-
   getContextManagementConfig(): ContextManagementConfig {
     return this.contextManagement;
   }
@@ -3782,10 +3778,6 @@ export class Config implements McpContext, AgentLoopContext {
       process.env['GEMINI_CLI_EXP_AGENT'] === 'true' ||
       this.agentSessionInteractiveEnabled
     );
-  }
-
-  getAgentSessionSubagentEnabled(): boolean {
-    return this.agentSessionSubagentEnabled;
   }
 
   /**
